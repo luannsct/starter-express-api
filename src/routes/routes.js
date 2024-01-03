24935365b9eaa1b14e667cb127c95d16ea94b947
luannsct <luan.nsct@gmail.com>
2024-01-03 13:39:58 -0300

    Atualizando index.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const { logarUsuario, cadastrarNovoUsuario } = require('../controllers/usuarios')
 const { validarLoginUsuario, validarCadastro, validarCorpoRequisicaoLogin } = require('../middlewares/usuarios')
 const { usuaroSchemaLogin } = require('../schemas/usuarios')
 const routes = express()
 
+routes.use(cors())
 
-routes.get('/', (req, res) => {
-    return res.json(`Olá Mundo!`)
-})
 routes.post('/sign', validarCadastro, cadastrarNovoUsuario)
 routes.post('/login', validarCorpoRequisicaoLogin(usuaroSchemaLogin), validarLoginUsuario, logarUsuario)
+
+routes.get('/', (req, res) => {
+    res.send('Olá Mundo!')
+})
 
 module.exports = { routes }

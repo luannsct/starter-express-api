808ff73dbbd760e16023b516811691e4fb3285d7
luannsct <luan.nsct@gmail.com>
2024-01-03 14:59:30 -0300

    Chamando validações

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const { logarUsuario, cadastrarNovoUsuario } = require('../controllers/usuarios')
-const { validarLoginUsuario, validarCadastro, validarCorpoRequisicaoLogin } = require('../middlewares/usuarios')
-const { usuaroSchemaLogin } = require('../schemas/usuarios')
+const { validarLoginUsuario, validarCadastro, validarCorpoRequisicaoLogin, validarCorpoRequisicaoCadastro } = require('../middlewares/usuarios')
+const { usuaroSchemaLogin, usuarioSchemaCadastro } = require('../schemas/usuarios')
 const routes = express()
 
 routes.use(cors())
 
-routes.post('/sign', validarCadastro, cadastrarNovoUsuario)
+routes.post('/sign', validarCorpoRequisicaoCadastro(usuarioSchemaCadastro), validarCadastro, cadastrarNovoUsuario)
 routes.post('/login', validarCorpoRequisicaoLogin(usuaroSchemaLogin), validarLoginUsuario, logarUsuario)
 
 routes.get('/', (req, res) => {

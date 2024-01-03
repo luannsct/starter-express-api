b28d065fa14f09af902048d31b449067061bebb2
luannsct <luan.nsct@gmail.com>
2024-01-03 14:11:51 -0300

    update

--- src/config/db/knex.js
+++ src/config/db/knex.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const knex = require('knex')({
     client: process.env.PG_CLIENT,
     connection: {
-        host: process.env.PG_HOST,
+        host: process.env.DB_URI,
         user: process.env.PG_USER,
         password: process.env.PG_PASSWORD,
         database: process.env.PG_DATABASE,

728a7a9dabf4dd0e2164068fdbc2c8c52875bcc4
luannsct <luan.nsct@gmail.com>
2024-01-03 14:37:01 -0300

    :wrench: chore: atualização configuração knex para conexctar ao ElephantSQL

--- src/config/db/knex.js
+++ src/config/db/knex.js
@@ -2,13 +2,8 @@ require('dotenv').config();
 
 const knex = require('knex')({
     client: process.env.PG_CLIENT,
-    connection: {
-        host: process.env.DB_URI,
-        user: process.env.PG_USER,
-        password: process.env.PG_PASSWORD,
-        database: process.env.PG_DATABASE,
-        ssl: { rejectUnauthorized: false }
-    }
+    connection: process.env.DB_URI,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = knex;

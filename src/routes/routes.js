0efc0c785c5806bad6b2887d21031583591a3d1e
luannsct <luan.nsct@gmail.com>
2024-01-06 18:24:07 -0300

    removendo erros

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const { logarUsuario, cadastrarNovoUsuario } = require('../controllers/usuarios')
-const { validarLoginUsuario, validarCadastro, validarCorpoRequisicaoLogin, validarCorpoRequisicaoCadastro } = require('../middlewares/usuarios')
 const { usuaroSchemaLogin, usuarioSchemaCadastro } = require('../schemas/usuarios')
 const { listarPropostasPorEmail, buscarFornecedorEnergia } = require('../controllers/fornecedores')
 const { validarCorpoRequisicaoNovaCotacao } = require('../middlewares/fornecedor')

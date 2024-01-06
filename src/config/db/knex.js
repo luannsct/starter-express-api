1117b43b628adcaabf00340eb220825703f0ca3a
luannsct <luan.nsct@gmail.com>
2024-01-06 17:32:47 -0300

    Commit correcao

--- src/config/db/knex.js
+++ src/config/db/knex.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
 
-const knex = require('knex')({
+const knexDeploy = require('knex')({
     client: process.env.PG_CLIENT,
     connection: process.env.DB_URI,
     ssl: { rejectUnauthorized: false }
 });
+const knexLocal = require('knex')({
+    client: process.env.PG_CLIENT,
+    connection: {
+        host: process.env.PG_HOST,
+        port: process.env.PG_PORT,
+        database: process.env.PG_DATABASE,
+        user: process.env.PG_USER,
+        password: process.env.PG_PASSWORD
+    }
 
-module.exports = knex;
+})
+module.exports = { knexLocal, knexDeploy };
--- src/controllers/fornecedores.js
+++ src/controllers/fornecedores.js
@@ -1,14 +1,14 @@
-const knex = require("../config/db/knex");
+const { knexDeploy: knex } = require("../config/db/knex");
 const novaProposta = async (req, res) => {
-
+    const { id_fornecedor, nomecliente, emailcliente, consumokwhmensal } = req
     try {
         const inserirDados = await knex('price').insert({ id_fornecedor, nomecliente, emailcliente, consumokwhmensal }).returning('*');
         if (inserirDados.length <= 0) {
             return res.status(500).json({ message: "Oops... Infelizmente não foi possivel enviar sua proposta, tente novamente mais tarde." });
         }
-        const { senha: _, ...usuario } = inserirDados[0]
+        return res.status(200).json({ message: "Dados Salvos com sucesso!" })
     } catch (error) {
-
+        return res.status(500).json({ erro: "Oops... Não deu certo sua solicitação tente novamente mais tarde." })
     }
 }
 const listarPropostasPorEmail = async (req, res) => {
@@ -21,8 +21,20 @@ const listarPropostasPorEmail = async (req, res) => {
     }
 }
 const buscarFornecedorEnergia = async (req, res) => {
-    const { nomecliente, emailcliente, consumokwhmensal } = req.body;
-    console.log(typeof (consumokwhmensal))
+    const { consumokwhmensal } = req.body;
+    const consumokwhmensalFloat = parseFloat(consumokwhmensal)
+    try {
+        //where khwminimo >= 200 and avaliacaomedia > 3 order by custokwh  desc LIMIT(5)
+        const resultado = await knex('supplier').where('khwminimo', '<=', consumokwhmensalFloat).orderBy('avaliacaomedia', 'desc').limit(6)
+        if (resultado <= 0) {
+            return res.status(404).json({ erro: "Não encontramos nenhum fornecedor que possa atendê-lo no momento." })
+        }
+        console.log(consumokwhmensalFloat, resultado)
+        return res.status(200).json(resultado);
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).json({ erro: "Oops... Estamos com problemas para processar sua solicitação, tente novamente mais tarde." })
+    }
 
 }
-module.exports = { listarPropostasPorEmail, buscarFornecedorEnergia }
+module.exports = { listarPropostasPorEmail, buscarFornecedorEnergia, novaProposta }
